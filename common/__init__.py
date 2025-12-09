b44dccad0e75415a61eea007bf670491704a5b6a
k11kik <143200648+k11kik@users.noreply.github.com>
2025-12-10 01:17:03 +0900

    Update via script

---
+++ common/__init__.py
@@ -0,0 +1,33 @@
+from .base import (
+    mathpy,
+    display,
+    path,
+    colored_logging
+)
+
+from . import (
+    spec,
+    orbit,
+    data_process,
+    coordinate,
+    cdf,
+    git,
+    dispersion,
+    pytplot,
+    quant,
+    util,
+    time
+)
+
+# from . import spec
+# from . import orbit
+# from . import data_process
+# from . import coordinate
+
+# from . import cdf
+# from . import git
+# from . import dispersion
+# from . import pytplot
+# from . import quant
+# from . import util
+# from . import time
