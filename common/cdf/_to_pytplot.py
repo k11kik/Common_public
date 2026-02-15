77244ddd2d3497ca18a64c21f3c4744d6c518297
k11kik <143200648+k11kik@users.noreply.github.com>
2026-02-16 02:36:57 +0900

    Add files via upload

---
+++ common/cdf/_to_pytplot.py
@@ -0,0 +1,25 @@
+"""
+cdf > pytplot.py
+"""
+
+from . import cdfdata
+from common import pytplot
+
+def to_pytplot_single_var(
+        cdffile, 
+        var_times, 
+        var,
+        var_freqs=None,
+        store_name=None
+    ):
+    if store_name is None:
+        store_name = var
+    times = cdfdata.get_data(cdffile, var_times)
+    var_data = cdfdata.get_data(cdffile, var)
+    if var_freqs is None:
+        pytplot.store_data(store_name, {'x': times, 'y': var_data})
+    else:
+        freqs = cdfdata.get_data(cdffile, var_freqs)
+        pytplot.store_data(store_name, {'x': times, 'y': var_data, 'v': freqs})
+    return
+
