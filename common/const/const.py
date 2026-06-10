d367a64f9e7a37b5cc51af857028e4c69043b12c
k11kik <143200648+k11kik@users.noreply.github.com>
2026-06-10 13:48:45 +0900

    Update via script

---
+++ common/const/const.py
@@ -0,0 +1,15 @@
+# units should be MKSA (m, kg, s, A)
+# significant digits should be at least three digits
+
+G = 6.674e-11 # gravitational constant [m3 kg-1 s-2] ([N m2 kg-2])
+AU = 1.496e11 # astronomical unit [m]
+
+# Sun
+MS = 1.989e30 # solar mass [kg]
+
+# Earth
+RE = 6371.2 * 1e3 # Earth radius [m]
+
+# Mercury
+RM = 2439.7 * 1e3 # Mercury radius [m]
+DM_semi_major = 57.91e9 # semi-major axis of Mercury's orbit [m]
