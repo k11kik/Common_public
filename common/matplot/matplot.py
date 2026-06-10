214ef35727da9dad6e5a7084281fbffe84daa766
k11kik <143200648+k11kik@users.noreply.github.com>
2026-06-10 13:49:12 +0900

    Update via script

---
+++ common/matplot/matplot.py
@@ -0,0 +1,20 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import os
+import shutil
+
+
+def clear_cache():
+    print('Clearing matplotlib cache ...')
+    cache_dir = mpl.get_cachedir()
+    if os.path.exists(cache_dir):
+        shutil.rmtree(cache_dir)
+        print("  ✔ Matplotlib font cache were removed")
+        print("  ※ Please restart the editer")
+
+
+def init():
+    plt.rcParams['font.family'] = 'Roboto Condensed'
+    plt.rcParams['font.size'] = 18
+    plt.rcParams['legend.fontsize'] = 18
+    return
