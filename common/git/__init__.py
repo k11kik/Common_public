458139a7337778c1b64a04fd6ffcc36fe21217d1
k11kik <143200648+k11kik@users.noreply.github.com>
2026-06-18 15:14:26 +0900

    Update via script

--- common/git/__init__.py
+++ common/git/__init__.py
@@ -1,4 +1,17 @@
+"""
+initial setting
+    Put '.github_token.json' in /Users/username/
+    >>> {
+    "github_token": "your_github_token"
+    }
+"""
+
+
 from .github_sync import (
     download,
     upload
+)
+
+from .github_token_loader import (
+    load_github_token
 )
